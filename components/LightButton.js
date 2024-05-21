9008fd49fc8141c40e63513996cde1b46f9e1e4c
Bill Clinton <bclinton@appunto.net>
2024-05-21 12:49:57 -0400

    feat: add picker to web component

--- components/LightButton.js
+++ components/LightButton.js
@@ -1,4 +1,5 @@
-import { setOnState } from '../services/Lights.js';
+import { setOnState, setColorState } from '../services/Lights.js';
+import { convertRGBToHex } from '../services/Util.js';
 
 export default class LightButton extends HTMLElement {
   constructor() {
@@ -15,23 +16,51 @@ export default class LightButton extends HTMLElement {
     this.root.appendChild(content);
 
     const button = this.root.querySelector('button');
+    const picker = this.root.querySelector('input[type=color]');
     const state = JSON.parse(this.getAttribute('lightState'));
 
     button.addEventListener('click', (event) => {
       setOnState(this.getAttribute('lightID'), !state.on);
     });
 
+    picker.addEventListener('input', (event) => {
+      setColorState(
+        this.getAttribute('lightID'),
+        event.target.value,
+        this.getAttribute('lightModelID'),
+        false
+      );
+    });
+
+    picker.addEventListener('change', (event) => {
+      setColorState(
+        this.getAttribute('lightID'),
+        event.target.value,
+        this.getAttribute('lightModelID')
+      );
+    });
+
     this.render();
   }
 
   render() {
+    const wrapper = this.root.querySelector('.wrapper');
     const button = this.root.querySelector('button');
     const picker = this.root.querySelector('input[type=color]');
     const name = this.getAttribute('lightName');
     const state = JSON.parse(this.getAttribute('lightState'));
+    const bg = convertRGBToHex(
+      window.getComputedStyle(wrapper).getPropertyValue('background-color')
+    );
 
     button.innerText = name + ' : ' + (state.on ? 'on' : 'off');
-    picker.value = this.getAttribute('lightColor');
+    if (state.on) {
+      picker.removeAttribute('disabled');
+      picker.value = this.getAttribute('lightColor');
+    } else {
+      picker.setAttribute('disabled', true);
+      picker.value = bg;
+    }
   }
 }
 
